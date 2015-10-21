50e5cc250ffff752065837b0b88b9908765b07e1
Alexey Golubev <dr.freecx@gmail.com>
2015-10-21 11:47:31 +0300

    update page rendering

--- render-page/js/main.js
+++ render-page/js/main.js
@@ -1,10 +1,33 @@
-var circle_color = '#123456';
 var geoJSON_layer = new L.geoJson();
 $.getJSON('./js/convex-hull.json', function(data) {
     geoJSON_layer = L.geoJson(data, {
         'color': '#1e90ff',
         'width': 5,
-        'opacity': 0.95
+        'opacity': 0.95,
+        onEachFeature: function(feature, layer) {
+            if (feature.properties) {
+                layer.bindPopup(feature.properties['label']);
+            }
+        },
+        pointToLayer: function (feature, latlng) {
+            if (feature.properties) {
+                return new L.CircleMarker(latlng, {
+                    radius: 5,
+                    color: '#B22222',
+                    weight: 1,
+                    opacity: 0.8,
+                    fillOpacity: 0.4
+                });
+            } else {
+                return new L.CircleMarker(latlng, {
+                    radius: 10,
+                    color: '#123456',
+                    weight: 1,
+                    opacity: 0.8,
+                    fillOpacity: 0.4
+                });
+            }
+        },
     });
     var map = L.map('map', {
         'center': [48.7900, 44.8000],
