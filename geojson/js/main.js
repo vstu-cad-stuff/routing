b9b8ffa87b129ba34fc7c50313cbadee68726e0b
Alexey Golubev <dr.freecx@gmail.com>
2015-11-03 22:03:43 +0300

    [upd] data & style

--- geojson/js/main.js
+++ geojson/js/main.js
@@ -1,9 +1,18 @@
 var geoJSON_layer = new L.geoJson();
 $.getJSON('./js/convex-hull.json', function(data) {
     geoJSON_layer = L.geoJson(data, {
-        'color': '#1e90ff',
-        'width': 5,
-        'opacity': 0.95,
+        style: function(feature) {
+            if (feature.properties.color) {
+                color = feature.properties.color;
+            } else {
+                color = '#123456';
+            }
+            return {
+                'color': color,
+                'width': 5,
+                'opacity': 0.95
+            };
+        },
         onEachFeature: function(feature, layer) {
             if (feature.properties) {
                 layer.bindPopup(feature.properties['label']);
@@ -17,16 +26,15 @@ $.getJSON('./js/convex-hull.json', function(data) {
                     color = '#AC66AF';
                 }
                 return new L.CircleMarker(latlng, {
-                    radius: 12,
+                    radius: 8,
                     color: color,
                     weight: 4,
-                    opacity: 0.5,
+                    opacity: 0.8,
                     fillOpacity: 0.8
                 });
             } else {
                 return new L.CircleMarker(latlng, {
                     radius: 8,
-                    color: '#123456',
                     weight: 1,
                     opacity: 1,
                     fillOpacity: 0.35
