6daeafa5c8340caeb31d2dd6e4810c3a2c395778
Alexey Golubev <dr.freecx@gmail.com>
2016-02-09 15:59:12 +0300

    [add] route length

--- geojson/js/main.js
+++ geojson/js/main.js
@@ -1,3 +1,5 @@
+var select_index = 0;
+var distance_sum = 0;
 var map = 0;
 var geoJSON_layer = 0;
 var layer = 0;
@@ -95,8 +97,8 @@ function load_data(data) {
         var slider = L.control({position: 'bottomleft'});
         slider.onAdd = function (map) {
           this._div = L.DomUtil.create('div', 'slider');
-          this._div.innerHTML = 
-            '<button id="decrement" onclick="present(-1);"><</button>' + 
+          this._div.innerHTML =
+            '<button id="decrement" onclick="present(-1);"><</button>' +
             '<h4 id="dataset">100 кластеров / 8 маршрутов</h4>' +
             '<button id="increment" onclick="present(+1);">></button>';
           return this._div;
@@ -104,16 +106,48 @@ function load_data(data) {
         slider.addTo(map);
         info.onAdd = function (map) {
           this._div = L.DomUtil.create('div', 'info');
-          this._div.innerHTML = '<h4>Выбранный объект</h4>';
+          this._div.innerHTML = '<h4>Выбранны объект</h4>';
           this.update();
           return this._div;
         };
         info.update = function (props) {
-          this._div.innerHTML = '<h4>Выбранный объект</h4>' + 
-            (props ? '<b>' + props.label + '</b>' : '<b>--</b>');
+          if (props) {
+            regexp_n = /[0-9]{1,}/;
+            regexp_a = /[a-zA-Z]{1,}/;
+            route_num = parseInt(regexp_n.exec(props.label)) - 2;
+            route_name = regexp_a.exec(props.label) + ' №' + (route_num + 1);
+          }
+          this._div.innerHTML = '<h4>Выбранный объект</h4>' +
+            (props ? '<b>' + route_name + '</b>' : '<b>--</b>') +
+            '<h4>Длина маршрута</h4>' +
+            '<b>' + (props ? distances[select_index][route_num] + ' м' : '--') + '</b>' +
+            '<h4>Суммарная длина</h4>' + '<b id="total_distance">' + distance_sum + ' м</b>';
         };
         info.addTo(map);
     }
     map.fitBounds(geoJSON_layer);
   });
 }
+
+function present(inc) {
+  var data_list = [
+      'result-100-8', 'result-100-10', 'result-100-12', 'result-100-14', 'result-100-16', 'result-100-18',
+      'result-150-8', 'result-150-10', 'result-150-12', 'result-150-14', 'result-150-16', 'result-150-18',
+      'result-200-8', 'result-200-10', 'result-200-12', 'result-200-14', 'result-200-16', 'result-200-18']
+  select_index += inc;
+  if (select_index < 0 ) {
+    select_index = data_list.length-1;
+  } else if (select_index > data_list.length-1) {
+    select_index = 0;
+  }
+  distance_sum = 0;
+  for (i = 0; i < distances[select_index].length; i++) {
+    distance_sum += distances[select_index][i];
+  }
+  selected_name = data_list[select_index];
+  load_data(data_list[select_index]);
+  dataset.innerHTML = selected_name.substr(7, 3) + ' кластеров / ' +
+    selected_name.substr(11, 3) + ' маршрутов';
+  total_distance.innerHTML = distance_sum + ' м';
+}
+present(0);
